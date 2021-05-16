d0d2cf99b4b453769e1e8ba35dcea8d3ba3efd09
Galiant <antonijodev@gmail.com>
2021-05-16 20:16:23 +0100

    useReducer hook example

--- react-refresh/src/App.js
+++ react-refresh/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactUseEffect from './ReactUseEffect/ReactUseEffect';
+import ReactUseEffect from './ReactHooks/ReactUseEffect';
+import ReactUseReducer from './ReactHooks/ReactUseReducer';
 
 function App() {
   return (
-    <div>
-      <h1>React useState And useEffect</h1>
-      <ReactUseEffect />
+    <div style={{ margin: '0 20px' }}>
+      <h1>React Refresh</h1>
+      <div>
+        <h2>React useState And useEffect</h2>
+        <ReactUseEffect />
+        <h2>React useReducer</h2>
+        <ReactUseReducer />
+      </div>
     </div>
   );
 }

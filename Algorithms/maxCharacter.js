09190cba8cc194741d7a89e479075902d23f22d9
Galiant <antonijodev@gmail.com>
2021-05-18 00:05:01 +0100

    Find max character in a string

---
+++ Algorithms/maxCharacter.js
@@ -0,0 +1,26 @@
+// MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') === 'a'
+
+function maxCharacter(str) {
+  const charMap = {};
+  let maxNum = 0;
+  let maxChar = '';
+  str.split('').forEach(char => {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+  for (let char in charMap) {
+    if (charMap[char] > maxNum) {
+      maxNum = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+console.log('MAX_CHARACTER', maxCharacter('javascript'));

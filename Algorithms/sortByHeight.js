94db072786a22137e0245048b6bd8bb09027ea93
Galiant <antonijodev@gmail.com>
2021-05-23 23:32:56 +0100

    Sort array by height while not moving static values

---
+++ Algorithms/sortByHeight.js
@@ -0,0 +1,25 @@
+// SORT BY HEIGHT
+// Some people are standing in a row in a park. These are trees between them which cannot be
+// moved. Your task is rearrange the people by their heights in a non-descending order
+// without moving the trees.
+// ex.
+// a = [-1, 150, 190, 170, -1, -1, 160, 180]
+// sortByHeight(a) === [-1, 150, 160, 170, -1, -1, 180, 190]
+
+function sortByHeight(a) {
+  const arr1 = [];
+  const arr2 = [];
+
+  a.forEach((val, i) => (val === -1 ? arr1.push(i) : arr2.push(val)));
+
+  const sortArray = arr2.sort((a, b) => a - b);
+
+  arr1.forEach((val, i) => sortArray.splice(arr1[i], 0, -1));
+
+  return sortArray;
+}
+
+console.log(
+  'SORT_BY_HEIGHT',
+  sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180])
+);

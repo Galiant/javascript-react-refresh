aa60ea6cfcc676d6760dce3367b7ce61866221e7
Galiant <antonijodev@gmail.com>
2021-06-12 23:39:08 +0100

    Setting up redux actions

--- redux-refresh/src/App.js
+++ redux-refresh/src/App.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from 'reducers';
+import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
